93bfa53439af3558e4ed970ea229f92e366c7dd7
jlyonas <jlyon@acumensolutions.com>
2017-09-20 18:38:09 -0400

    mapbox polygon poc

--- app/assets/js/mapbox-api.js
+++ app/assets/js/mapbox-api.js
@@ -5,14 +5,59 @@ const config = {
 	url: "https://api.mapbox.com/v4/mapbox.emerald/page.html?access_token=",
 	mapContainer: "mapbox",
 	styleUrl: "mapbox://styles/mapbox/streets-v9",
+
+	center: [-77.036, 38.895],
+	zoom: 12,
+};
+
+const createMap = function() {
+	mapboxgl.accessToken = config.pkey;
+	return new mapboxgl.Map({
+		container: config.mapContainer,
+		style: config.styleUrl,
+		center: config.center,
+		zoom: config.zoom,
+	});;
+};
+
+const createGeocoder = function() {
+	return new MapboxGeocoder({
+		accessToken: config.pkey,
+	});
+};
+
+const createDraw = function() {
+	return new MapboxDraw({
+		displayControlsDefault: false,
+		controls: {
+			polygon: true,
+			trash: true,
+		},
+	});
+};
+
+const attachCalcClickHandler = function(draw) {
+	let calcBtn = document.getElementById("calculate");
+	calcBtn.addEventListener("click", function() {
+		let data = draw.getAll();
+		if(data.features.length > 0) {
+			let area = turf.area(data);
+			let roundedArea = Math.round(area*100)/100;
+			let answer = document.getElementById("calculated-area");
+			answer.innerHTML = "<p><strong>" + roundedArea + "</strong><p><p>square meters</p>";
+		}
+	});
 };
 
 const init = function() {
-	mapboxgl.accessToken = config.pkey;
-	var map = new mapboxgl.Map({
-		container: config.mapContainer,
-		style: config.styleUrl,
-	});
+	let map = createMap();
+	let geocoder = createGeocoder();
+	let draw = createDraw();
+
+	map.addControl(geocoder);
+	map.addControl(draw);
+
+	attachCalcClickHandler(draw);
 };
 
 init();
