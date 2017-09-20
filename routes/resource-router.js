89dac9f60341ad7648a105a5b0040bda9b5e46a4
jlyonas <jlyon@acumensolutions.com>
2017-09-20 13:07:56 -0400

    correct routes

--- routes/resource-router.js
+++ routes/resource-router.js
@@ -4,15 +4,15 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/css/master.css", function(req, res) {
-	res.sendFile( path.join(__dirname, "app", "assets", "css", "master.css"));
+	res.sendFile( path.join(__dirname, "../app", "assets", "css", "master.css"));
 });
 
 router.get("/js/app.js", function(req, res) {
-	res.sendFile( path.join(__dirname, "assets", "js", "app.js"));
+	res.sendFile( path.join(__dirname, "../app", "assets", "js", "app.js"));
 });
 
 router.get("/js/google-places-api.js", function(req, res) {
-	res.sendFile( path.join(__dirname, "assets", "js", "google-places-api.js"));
+	res.sendFile( path.join(__dirname, "../app", "assets", "js", "google-places-api.js"));
 });
 
 module.exports = router;
